ccb46c927c59c23df973ce49fd06707fe5b78357
Katrin Krieger <contact@twinrivercode.de>
2018-01-18 16:00:52 +0100

    c

--- index.js
+++ index.js
@@ -7,4 +7,4 @@ const
   app = express().use(bodyParser.json()); //create express http server
   
 //set server port and log message on success
-app.listen(process.env..PORT || 1337), () => console.log('webhook is listening');
+app.listen(process.env.PORT || 1337), () => console.log('webhook is listening');
