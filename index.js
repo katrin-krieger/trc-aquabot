cb4a090c40187728a064671a1e4175e651debd6b
Katrin Krieger <contact@twinrivercode.de>
2018-01-18 15:44:23 +0100

    restart project

---
+++ index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+//import deps and setup http server
+const 
+  express = require('express'),
+  bodyParser = require('body-parser'),
+  app = express().use(bodyParser.json()); //create express http server
+  
+//set server port and log message on success
+app.listen(process.env..PORT || 1337), () => console.log('webhook is listening');
